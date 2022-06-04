3d1edff9b433503137d0b62faa80e38fdd6b4707
Hemant K <hvlk369@gmail.com>
2022-06-05 01:03:47 +0530

    commit code

---
+++ src/main/java/com/hk/obf/demo/ObfappApplication.java
@@ -0,0 +1,13 @@
+package com.hk.obf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObfappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObfappApplication.class, args);
+	}
+
+}
